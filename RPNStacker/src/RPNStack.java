bfa342e79d1f3ef5397dd90563588e90ce4fbcb0
Alexandra <avz@cin.ufpe.br>
2021-11-23 19:35:18 -0300

    implementa lista de tokens

--- RPNStacker/src/RPNStack.java
+++ RPNStacker/src/RPNStack.java
@@ -10,40 +10,72 @@ public class RPNStack {
 		// TODO Auto-generated method stub
 		Scanner in = new Scanner(openFile());
 
+		ArrayList<Token> tokens = new ArrayList<Token>();
 		Stack<Integer> stack = new Stack<>();
 
 		int currentX, currentY; //expressao no formato "x <operador> y
 		char currentOperator;
+		char currentSymbol;
 
 
 		while (in.hasNext()) {
 
 			if(in.hasNextInt()) {
-				int inteiro = in.nextInt();
-				stack.push(inteiro);
+				//int inteiro = in.nextInt();				
+				//stack.push(inteiro);
+				String numero = String.valueOf(in.nextInt()); 
+				Token numToken = new Token(TokenType.NUM, numero);
+				tokens.add(numToken);
 
 			}else {
-
-				currentOperator = in.next().charAt(0);
-				currentY = stack.pop();
-				currentX = stack.pop();
-
-				
-				if (currentOperator == '+') {				
-					stack.push(currentX + currentY);
+				currentSymbol = in.next().charAt(0);
+		
+				if (currentSymbol == '+') {	
 					
-				}else if (currentOperator == '-') {
-					stack.push(currentX - currentY);
+					Token plusToken = new Token(TokenType.PLUS, String.valueOf(currentSymbol));
+					tokens.add(plusToken);
 					
-				}else if (currentOperator == '*') {
-					stack.push(currentX * currentY);
+				}else if (currentSymbol == '-') {
+					
+					Token minusToken = new Token(TokenType.MINUS, String.valueOf(currentSymbol));
+					tokens.add(minusToken);
+					
+				}else if (currentSymbol == '*') {
+					
+					Token starToken = new Token(TokenType.STAR, String.valueOf(currentSymbol));
+					tokens.add(starToken);
+					
+				}else if  (currentSymbol == '/'){
+					
+					Token slashToken = new Token(TokenType.SLASH, String.valueOf(currentSymbol));
+					tokens.add(slashToken);
 					
 				}else {
-					stack.push(currentX / currentY);
+					System.out.println("error");
 				}
+				
+//				currentOperator = in.next().charAt(0);
+//				currentY = stack.pop();
+//				currentX = stack.pop();
+//
+//				
+//				if (currentOperator == '+') {				
+//					stack.push(currentX + currentY);
+//					
+//				}else if (currentOperator == '-') {
+//					stack.push(currentX - currentY);
+//					
+//				}else if (currentOperator == '*') {
+//					stack.push(currentX * currentY);
+//					
+//				}else {
+//					stack.push(currentX / currentY);
+//				}
+			
 			}
 		}
-		System.out.println("the answer is " + stack.pop());
+		//System.out.println("the answer is " + stack.pop());
+		System.out.println(tokens);
 
 	}
 
