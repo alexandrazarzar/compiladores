b2aa8f0f39d41cedd1503976cf19b9296c847ddc
Alexandra <avz@cin.ufpe.br>
2021-11-23 20:16:21 -0300

    retorna lista de tokens e resposta final

--- RPNStacker/src/RPNStack.java
+++ RPNStacker/src/RPNStack.java
@@ -21,62 +21,68 @@ public class RPNStack {
 		while (in.hasNext()) {
 
 			if(in.hasNextInt()) {
-				//int inteiro = in.nextInt();				
-				//stack.push(inteiro);
 				String numero = String.valueOf(in.nextInt()); 
 				Token numToken = new Token(TokenType.NUM, numero);
 				tokens.add(numToken);
 
 			}else {
 				currentSymbol = in.next().charAt(0);
-		
+
 				if (currentSymbol == '+') {	
-					
+
 					Token plusToken = new Token(TokenType.PLUS, String.valueOf(currentSymbol));
 					tokens.add(plusToken);
-					
+
 				}else if (currentSymbol == '-') {
-					
+
 					Token minusToken = new Token(TokenType.MINUS, String.valueOf(currentSymbol));
 					tokens.add(minusToken);
-					
+
 				}else if (currentSymbol == '*') {
-					
+
 					Token starToken = new Token(TokenType.STAR, String.valueOf(currentSymbol));
 					tokens.add(starToken);
-					
+
 				}else if  (currentSymbol == '/'){
-					
+
 					Token slashToken = new Token(TokenType.SLASH, String.valueOf(currentSymbol));
 					tokens.add(slashToken);
-					
+
 				}else {
 					System.out.println("error");
 				}
-				
-//				currentOperator = in.next().charAt(0);
-//				currentY = stack.pop();
-//				currentX = stack.pop();
-//
-//				
-//				if (currentOperator == '+') {				
-//					stack.push(currentX + currentY);
-//					
-//				}else if (currentOperator == '-') {
-//					stack.push(currentX - currentY);
-//					
-//				}else if (currentOperator == '*') {
-//					stack.push(currentX * currentY);
-//					
-//				}else {
-//					stack.push(currentX / currentY);
-//				}
-			
 			}
 		}
-		//System.out.println("the answer is " + stack.pop());
-		System.out.println(tokens);
+		for (int i = 0; i < tokens.size(); i++) {
 
+			Token currentToken = tokens.get(i);
+			TokenType currentType = currentToken.type;
+			System.out.println(currentToken.toString());
+
+			if (currentType == TokenType.NUM) {
+				int inteiro = Integer.parseInt(currentToken.lexeme);				
+				stack.push(inteiro);
+				
+			}else {
+				currentY = stack.pop();
+				currentX = stack.pop();
+				
+				if (currentType == TokenType.PLUS){
+					stack.push(currentX + currentY);
+					
+				}else if (currentType == TokenType.MINUS){
+					stack.push(currentX - currentY);
+					
+				}else if (currentType == TokenType.STAR){
+					stack.push(currentX * currentY);
+					
+				}else if (currentType == TokenType.SLASH){
+					stack.push(currentX / currentY);
+				}
+			}
+		}
+		
+		System.out.println("\nThe final answer is " + stack.pop());
 	}
 
 	private static File openFile() {
